0f68b567bff18809ae2ec627c946ae937458bdbc
Vinson Chuong <vinsonchuong@gmail.com>
2015-08-24 22:29:41 -0700

    Extract minimal Rails recipe from project_bootstrap

---
+++ lib/rails/template.rb
@@ -0,0 +1,81 @@
+def bundle(command)
+  Bundler.with_clean_env do
+    run "bundle #{command}"
+  end
+end
+
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+
+ruby '#{ENV['RUBY_VERSION']}'
+gem 'rails', '#{ENV['RAILS_VERSION']}'
+
+gem 'pg'
+gem 'puma'
+
+gem 'sass-rails'
+gem 'jquery-rails'
+gem 'uglifier'
+
+group :development do
+  gem 'spring'
+  gem 'spring-watcher-listen'
+  gem 'spring-commands-rspec'
+
+  gem 'pry-rails'
+  gem 'web-console'
+end
+
+group :development, :test do
+  gem 'byebug'
+  gem 'pry-byebug'
+
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'poltergeist'
+  gem 'selenium-webdriver'
+end
+RUBY
+
+bundle 'update'
+
+copy_file 'app/assets/stylesheets/application.css', 'app/assets/stylesheets/application.css.scss'
+remove_file 'app/assets/stylesheets/application.css'
+gsub_file 'app/assets/stylesheets/application.css.scss', /^.*=require.*\n/, ''
+
+generate 'rspec:install'
+
+insert_into_file 'spec/rails_helper.rb', <<-RUBY, after: /^# Add additional.*\n/
+require 'capybara/rails'
+require 'capybara/poltergeist'
+Capybara.default_driver = :poltergeist
+#Capybara.default_driver = :selenium
+RUBY
+
+comment_lines 'spec/rails_helper.rb', /use_transactional_fixtures/
+
+insert_into_file 'spec/rails_helper.rb', <<-RUBY, after: /use_transactional_fixtures.*\n/
+  config.before { DatabaseCleaner.strategy = :transaction }
+  config.before(type: :feature) { DatabaseCleaner.strategy = :truncation }
+  config.before { DatabaseCleaner.start }
+  config.after { DatabaseCleaner.clean }
+RUBY
+
+gsub_file 'spec/spec_helper.rb', "=begin\n", ''
+gsub_file 'spec/spec_helper.rb', "=end\n", ''
+
+file '.travis.yml', <<-YAML
+---
+language: ruby
+rvm:
+  - #{ENV['RUBY_VERSION']}
+YAML
+
+bundle 'binstubs rspec-core'
+bundle 'exec spring binstub --all'
+rake 'db:create db:migrate'
--- spec/rails.bats
+++ spec/rails.bats
@@ -1,24 +1,36 @@
 #!/usr/bin/env bats
 
 setup() {
-  echo
-}
-
-teardown() {
-  rm -rf '/tmp/awesome_blog'
-  gem clean &>/dev/null
+  export GEM_HOME='/tmp/awesome_blog/.gem'
 }
 
 @test 'it can bootstrap a Rails app' {
-  run lib/rails/install '/tmp/awesome_blog'
+  run prototypical rails '/tmp/awesome_blog'
+  echo "$output"
   [[ $status = 0 ]]
   [[ $output = *'Successfully installed bundler'* ]]
   [[ -d '/tmp/awesome_blog' ]]
-  [[ -f '/tmp/awesome_blog/Gemfile' ]]
-  skip
 
-  cd '/tmp/awesome_blog'
-  mkfifo server
-  ./bin/rails server &>server
-  cat server
+  pushd '/tmp/awesome_blog' &>/dev/null
+  run bin/rails generate model article title:string
+  run bin/rake db:migrate
+  run bin/rails runner 'Article.create(title: "First Article")'
+  run bin/rails runner 'puts Article.first.title'
+  [[ $output = 'First Article' ]]
+  popd &>/dev/null
 }
+
+teardown() {
+  if [[ -d '/tmp/awesome_blog' ]]
+  then
+    pushd '/tmp/awesome_blog'
+    bin/spring stop
+    popd
+    rm -rf '/tmp/awesome_blog'
+  fi
+
+  gem clean &>/dev/null
+  dropdb --if-exists 'awesome_blog_development'
+  dropdb --if-exists 'awesome_blog_test'
+}
+
