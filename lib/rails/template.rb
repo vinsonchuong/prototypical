fd5e514417735f41ca6aa3b8463fe13fe0def96a
Vinson Chuong <vinsonchuong@gmail.com>
2015-08-27 22:09:50 -0700

    Add placeholder for readme and license to Rails recipe

--- lib/rails/template.rb
+++ lib/rails/template.rb
@@ -69,7 +69,7 @@ RUBY
 gsub_file 'spec/spec_helper.rb', "=begin\n", ''
 gsub_file 'spec/spec_helper.rb', "=end\n", ''
 
-file '.travis.yml', <<-YAML
+file '.travis.yml', <<'YAML'
 ---
 language: ruby
 rvm:
@@ -84,5 +84,39 @@ YAML
 bundle 'binstubs rspec-core'
 bundle 'exec spring binstub --all'
 rake 'db:create db:migrate db:test:prepare'
+
+remove_file 'README.rdoc'
+file 'README.md', <<'MARKDOWN'
+# Name of App
+Travis CI Badge
+
+## Section 1
+
+## Section 2
+MARKDOWN
+
+file 'LICENSE', <<'TEXT'
+The MIT License (MIT)
+
+Copyright (c) 2015 Vinson Chuong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+TEXT
 
 git :init
